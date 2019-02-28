f14a49b96bcce98339e3386f6845bb98e0f604fd
Koen Vlaswinkel <koen@vlaswinkel.info>
2019-02-28 16:34:06 +0100

    Initial web server setup

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"mime"
+	"net/http"
 	"os"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/gin-gonic/gin"
 
 	_ "github.com/lib/pq"
 )
@@ -26,4 +29,29 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+	// Set up our Gin router
+	r := gin.Default()
+
+	// Load templates from the templates directory
+	r.LoadHTMLGlob("templates/*")
+	// Make sure we serve .js files as JavaScript
+	mime.AddExtensionType(".js", "application/javascript")
+	// Serve static files from the static directory
+	r.Static("/static", "./static")
+
+	// Set up two groups, which we’ll be able to apply middleware to later on for easier handling
+	unauthenticated := r.Group("/")
+	authenticated := r.Group("/")
+
+	// Add a default web page, which will become our homepage
+	authenticated.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello world!")
+	})
+
+
+	unauthenticated.GET("/login", LoginHandler())
+
+	// Run our web server on port 8080
+	log.Fatal(r.Run(":8080"))
 }
